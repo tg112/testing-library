6e0fcf68cecbb15ad0d389f48aee67e19e0a47c8
tg112 <info.tgw112@gmail.com>
2023-02-26 20:12:11 +0900

    done section9

--- codesplain-starter/src/routes/HomeRoute.test.js
+++ codesplain-starter/src/routes/HomeRoute.test.js
@@ -3,36 +3,34 @@ import { setupServer } from 'msw/node';
 import { rest } from "msw";
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
+import { createServer } from "../test/server";
 
-const hanlders = [
-  rest.get('/api/repositories', (req, res, ctx) => {
-    const language = req.url.searchParams.get('q').split('language:')[1];
-    console.log(language);
+// Goal
 
-    return res(
-      ctx.json({
+createServer([
+  {
+    path: '/api/repositories',
+    method: 'get',
+    res: (req, res, ctx) => {
+      const language = req.url.searchParams.get('q').split('language:')[1];
+      return {
         items: [
           { id: 1, full_name: `${language}_one` },
           { id: 2, full_name: `${language}_two` }
         ]
-      })
-    )
-  })
-];
-
-const server = setupServer(...hanlders);
-
-beforeAll(() => {
-  server.listen();
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-
-afterAll(() => {
-  server.close();
-});
+      }
+    }
+  },
+  // {
+  //   path: '/api/repositories',
+  //   method: 'post',
+  //   res: (req, res, ctx) => {
+  //     return {
+  //       items: [{}, {}, {}]
+  //     }
+  //   }
+  // }
+]);
 
 test('renders two links for each language', async () => {
   render(
@@ -62,8 +60,4 @@ test('renders two links for each language', async () => {
     expect(links[0]).toHaveAttribute('href', `/repositories/${language}_one`);
     expect(links[1]).toHaveAttribute('href', `/repositories/${language}_two`);
   };
-});
-
-const pause = new Promise(resolve => {
-  setTimeout(resolve, 100)
 });
